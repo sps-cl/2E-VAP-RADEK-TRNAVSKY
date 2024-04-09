6503817e2c22ec71e19f830d719de061622369b4
Radek Trnavský <147998859+RadekTrnavsky@users.noreply.github.com>
2024-04-09 08:03:28 +0200

    Add files via upload

---
+++ úkoly/KonzoloveAplikace/KonzApp4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TextEditor
+{
+    private List<string> words;
+    private int currentIndex;
+
+    public TextEditor()
+    {
+        words = new List<string>();
+        currentIndex = -1;
+    }
+
+    public void AddWord(string word)
+    {
+        words.Add(word);
+        currentIndex = words.Count - 1;
+        Console.WriteLine(word);
+    }
+
+    public void ShowPreviousWord()
+    {
+        if (currentIndex > 0)
+            currentIndex--;
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    public void ShowNextWord()
+    {
+        if (currentIndex < words.Count - 1)
+            currentIndex++;
+        Console.WriteLine(words[currentIndex]);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TextEditor editor = new TextEditor();
+
+        while (true)
+        {
+            string command = Console.ReadLine().Trim();
+
+            if (command.StartsWith("Pridat:".ToLower()))
+            {
+                string word = command.Split(':')[1];
+                editor.AddWord(word);
+            }
+            else if (command == "Zpet".ToLower())
+            {
+                editor.ShowPreviousWord();
+            }
+            else if (command == "Vpred".ToLower())
+            {
+                editor.ShowNextWord();
+            }
+            else
+            {
+                Console.WriteLine("Neznamy prikaz.");
+            }
+        }
+    }
+}
+
+
+
