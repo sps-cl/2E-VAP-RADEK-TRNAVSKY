a0fb87b3ffcad45ffc18d614387a88a9d4bd0615
Radek Trnavský <147998859+RadekTrnavsky@users.noreply.github.com>
2024-05-24 16:21:48 +0200

    Add files via upload

---
+++ úkoly/KonzoloveAplikace/KonzApp5/KonzApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Queue<char> pismena = new Queue<char>();
+        static int pocetZnaku = 0;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Nedostatečný počet argumentů");
+                return;
+            }
+            string path = args[0];
+            if (File.Exists(path))
+            {
+                StreamReader reader = new StreamReader(path);
+
+                using (StreamWriter writer = new StreamWriter(args[1], false))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        char pismeno = (char)reader.Read();
+                        pismena.Enqueue(pismeno);
+                        pocetZnaku++;
+                        writer.WriteLine($"{pismena.Dequeue()}");
+                    }
+                    writer.WriteLine($"Počet znaků je: {pocetZnaku}");
+                }
+            }
+        }
+    }
+}
